53b59baaf8502d2803b84b1e1dc50a3584be184f
Prince0205 <44364707+Prince0205@users.noreply.github.com>
2018-10-23 16:02:50 +0530

    Update file1.tf

--- file1.tf
+++ file1.tf
@@ -1,10 +1,1 @@
-provider "aws" {
-    access_key = "AKIAIYDDQPX6NXDLFQSQ"
-    secret_key = "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"
-    region = "us-east-2"
-}
 
-resource "aws_instance" "testserver" {
-    ami = "ami-0b59bfac6be064b78"
-    instance_type = "t2.micro"
-}
