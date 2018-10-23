0b36d6443b2df3e300d5932a25a57eeb083e1410
Prince0205 <44364707+Prince0205@users.noreply.github.com>
2018-10-23 13:14:24 +0530

    Update file1.tf

--- file1.tf
+++ file1.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-    access_key = " AKIAIYDDQPX6NXDLFQSQ "
+    access_key = "AKIAIYDDQPX6NXDLFQSQ"
     secret_key = "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"
     region = "us-east-2"
 }
