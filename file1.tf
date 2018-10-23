ee2967db8f731cf71f7eb61dbd0043dde357523a
Prince0205 <44364707+Prince0205@users.noreply.github.com>
2018-10-23 12:52:37 +0530

    Update file1.tf

--- file1.tf
+++ file1.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-    access_key = "AKIAICNTIN2NPPGE35AA"
-    secret_key = "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"
+    access_key = "AKIAJIGNMWMTFDTGOCMA"
+    secret_key = "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"
     region = "us-east-2"
 }
 
-resource "aws_instance" "server1" {
+resource "aws_instance" "testserver" {
     ami = "ami-0b59bfac6be064b78"
     instance_type = "t2.micro"
 }
